77e080a609444f63665afc7ff6000467782a910a
Vaijayanti Vasam <vaijayanti.vasam@biz4solutions.com>
2022-10-12 16:16:48 +0530

    add 2 numbers

--- Calculator.java
+++ Calculator.java
@@ -2,9 +2,15 @@ public class Calculator{
     void print(){
         System.out.println("Main Function");
     }
+
+    int addition(int x,int y){
+        return x+y;
+    }
+
     public static void main(String[] args) { 
         Calculator c = new Calculator();
         c.print();
+        System.out.println(c.addition(10, 20));
 
     }
 }
