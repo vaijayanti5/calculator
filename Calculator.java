39524db9f859bfcf8ab7a0db0936f17ffd77de80
Vaijayanti Vasam <vaijayanti.vasam@biz4solutions.com>
2022-10-12 16:10:56 +0530

    Initial Commit

---
+++ Calculator.java
@@ -0,0 +1,10 @@
+public class Calculator{
+    void print(){
+        System.out.println("Main Function");
+    }
+    public static void main(String[] args) { 
+        Calculator c = new Calculator();
+        c.print();
+
+    }
+}
