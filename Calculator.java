83555afaff4d41ae8301e0f6a8e213a13d278517
Vaijayanti Vasam <vaijayanti.vasam@biz4solutions.com>
2022-10-12 16:28:18 +0530

    Divide 2 numbers

--- Calculator.java
+++ Calculator.java
@@ -2,9 +2,14 @@ public class Calculator{
     void print(){
         System.out.println("Main Function");
     }
+
+    double divide(int x,int y){
+        return x/y;
+    }
+
     public static void main(String[] args) { 
         Calculator c = new Calculator();
         c.print();
-
+        System.out.println(c.divide(76, 2));
     }
 }
