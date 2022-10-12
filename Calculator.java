19285b7f867250d743b735d294cfed0e66f2419a
Vaijayanti Vasam <vaijayanti.vasam@biz4solutions.com>
2022-10-12 16:20:30 +0530

    Subtract 2 numbers

--- Calculator.java
+++ Calculator.java
@@ -2,9 +2,14 @@ public class Calculator{
     void print(){
         System.out.println("Main Function");
     }
+
+    int subtract(int x,int y){
+        return x-y;
+    }
+
     public static void main(String[] args) { 
         Calculator c = new Calculator();
         c.print();
-
+        System.out.println(c.subtract(10, 4));
     }
 }
