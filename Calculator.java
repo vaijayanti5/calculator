5ff546b61f4ae42e4ca073c7f0b7eec495c01bec
Vaijayanti Vasam <vaijayanti.vasam@biz4solutions.com>
2022-10-12 16:25:15 +0530

    Multiply 2 numbers

--- Calculator.java
+++ Calculator.java
@@ -2,9 +2,13 @@ public class Calculator{
     void print(){
         System.out.println("Main Function");
     }
+
+    int multiply(int x,int y){
+        return x*y;
+    }
     public static void main(String[] args) { 
         Calculator c = new Calculator();
         c.print();
-
+        System.out.println(c.multiply(10, 7));
     }
 }
